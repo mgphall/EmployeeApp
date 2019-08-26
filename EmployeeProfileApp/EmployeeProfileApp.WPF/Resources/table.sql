60e5eb6701219a6290aa29f2ca8e9cb39fc9f030
Unknown <mgphall@btconnect.com>
2019-08-26 23:29:03 +0100

    Added Sql file

---
+++ EmployeeProfileApp/EmployeeProfileApp.WPF/Resources/table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE "EmployeeProfile" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_EmployeeProfile" PRIMARY KEY AUTOINCREMENT,
+    "FirstName" TEXT NULL,
+    "LastName" TEXT NULL,
+    "Salary" TEXT NOT NULL,
+    "HoursPerDay" INTEGER NOT NULL,
+    "DaysPerWeek" INTEGER NOT NULL,
+    "WeeksPerYear" INTEGER NOT NULL
+)
