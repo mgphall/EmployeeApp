9732dd25b4dee014a255a1644a6d43dce970285d
Unknown <mgphall@btconnect.com>
2019-08-27 10:13:59 +0100

    updates

--- EmployeeProfileApp.Core.Tests/Tests/AddEmployeeViewModelTests.cs
+++ EmployeeProfileApp.Core.Tests/Tests/AddEmployeeViewModelTests.cs
@@ -21,6 +21,11 @@
             {
                 yield return new TestCaseData(new EmployeeProfile { FirstName = string.Empty, LastName = "Hall", Salary = 10000, DaysPerWeek = 1, HoursPerDay = 1, WeeksPerYear = 1 }).SetName("FirstName");
                 yield return new TestCaseData(new EmployeeProfile { FirstName = "Matthew", LastName = string.Empty, Salary = 1, HoursPerDay = 1, DaysPerWeek = 1, WeeksPerYear = 1 }).SetName("LastName");
+                yield return new TestCaseData(new EmployeeProfile { FirstName = "123456", LastName = "Hall", Salary = 10000, DaysPerWeek = 1, HoursPerDay = 1, WeeksPerYear = 1 }).SetName("NumbersFirstName");
+                yield return new TestCaseData(new EmployeeProfile { FirstName = "MatthewHall", LastName = "123", Salary = 10000, DaysPerWeek = 1, HoursPerDay = 1, WeeksPerYear = 1 }).SetName("NumbersLastName");
+                yield return new TestCaseData(new EmployeeProfile { FirstName = "MatthewHallMatthewHallMatthewHallMatthewHall", LastName = "Hall", Salary = 10000, DaysPerWeek = 1, HoursPerDay = 1, WeeksPerYear = 1 }).SetName("MaxLenghtFirstName");
+                yield return new TestCaseData(new EmployeeProfile { FirstName = "Matthew", LastName = "MatthewHallMatthewHallMatthewHallMatthewHall", Salary = 10000, DaysPerWeek = 1, HoursPerDay = 1, WeeksPerYear = 1 }).SetName("NumbersLastName");
+                yield return new TestCaseData(new EmployeeProfile { FirstName = "Matthew", LastName = string.Empty, Salary = 1, HoursPerDay = 1, DaysPerWeek = 1, WeeksPerYear = 1 }).SetName("LastName");
                 yield return new TestCaseData(new EmployeeProfile { FirstName = "Matthew", LastName = "Hall", Salary = 0, HoursPerDay = 1, DaysPerWeek = 1, WeeksPerYear = 1 }).SetName("Salary");
                 yield return new TestCaseData(new EmployeeProfile { FirstName = "Matthew", LastName = "Hall", Salary = 1, HoursPerDay = 0, DaysPerWeek = 1, WeeksPerYear = 1 }).SetName("HoursPerDayLow");
                 yield return new TestCaseData(new EmployeeProfile { FirstName = "Matthew", LastName = "Hall", Salary = 1, HoursPerDay = 25, DaysPerWeek = 1, WeeksPerYear = 1 }).SetName("HoursPerDayHigh");
--- EmployeeProfileApp/EmployeeProfileApp.Core/Models/Validation/EmployeeProfileValidator.cs
+++ EmployeeProfileApp/EmployeeProfileApp.Core/Models/Validation/EmployeeProfileValidator.cs
@@ -14,16 +14,51 @@
         private readonly string lettersAndSpaces = @"^[a-zA-Z]+(\s[a-zA-Z]+)?$";
 
         /// <summary>
+        /// The scale
+        /// </summary>
+        private readonly int scale =2;
+
+        /// <summary>
+        /// The precision
+        /// </summary>
+        private readonly int precision = 12;
+
+        /// <summary>
+        /// The maximum hours
+        /// </summary>
+        private readonly int maxHours = 24;
+
+        /// <summary>
+        /// The maximum days
+        /// </summary>
+        private readonly int maxDays = 7;
+
+        /// <summary>
+        /// The maximum weeks
+        /// </summary>
+        private readonly int maxWeeks = 52;
+
+        /// <summary>
+        /// The maximum length
+        /// </summary>
+        private readonly int maxLength = 35;
+
+        /// <summary>
+        /// The minimum length
+        /// </summary>
+        private readonly int minLength = 1;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="EmployeeProfileValidator"/> class.
         /// </summary>
         public EmployeeProfileValidator()
         {
-            RuleFor(x => x.FirstName).Length(1, 20).NotEmpty().Matches(lettersAndSpaces);
-            RuleFor(x => x.LastName).Length(1, 20).NotEmpty().Matches(lettersAndSpaces);
-            RuleFor(x => x.Salary).ScalePrecision(2,20).GreaterThan(0);
-            RuleFor(x => x.HoursPerDay).InclusiveBetween(1, 24);
-            RuleFor(x => x.DaysPerWeek).InclusiveBetween(1, 7);
-            RuleFor(x => x.WeeksPerYear).InclusiveBetween(1, 52);
+            RuleFor(x => x.FirstName).Length(minLength, maxLength).NotEmpty().Matches(lettersAndSpaces);
+            RuleFor(x => x.LastName).Length(minLength, maxLength).NotEmpty().Matches(lettersAndSpaces);
+            RuleFor(x => x.Salary).ScalePrecision(scale, precision).GreaterThan(0);
+            RuleFor(x => x.HoursPerDay).InclusiveBetween(1, maxHours);
+            RuleFor(x => x.DaysPerWeek).InclusiveBetween(1, maxDays);
+            RuleFor(x => x.WeeksPerYear).InclusiveBetween(1, maxWeeks);
         } 
     }
 }
